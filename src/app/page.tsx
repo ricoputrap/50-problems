92c8fa53aaf51d47cce0088a583ad9b2289611bf
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-04-24 06:15:45 +0700

    [PROB-6] feat: implement button Add Problem on the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
       {/* Top 50 problems ... */}
       <div className="flex justify-center py-7">
         <div className="max-w-80">
-          <h1 className="text-center text-3xl font-bold text-primary">
+          <h1 className="text-center text-3xl font-bold">
             Top 50 problems<br />
             reported by you<br />
             and ready to solve by<br />
@@ -16,6 +17,11 @@ export default function Home() {
       </div>
 
       {/* Button [+ Add Problem] */}
+      <div className="flex justify-center">
+        <Button className="font-bold">
+          + Add Problem
+        </Button>
+      </div>
 
       {/* Tab - view type selector */}
 
