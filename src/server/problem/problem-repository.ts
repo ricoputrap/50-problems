3e1fc7881e0a8dd84ca2167b3bcb2ec34bd2fe2c
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-04-30 06:02:34 +0700

    [PROB-14] feat: adjust the getTopUpvoted() to return the results in DESC order by created_at when two problems have the same upvote count

--- src/server/problem/problem-repository.ts
+++ src/server/problem/problem-repository.ts
@@ -5,11 +5,20 @@ import { problem as problemTable } from "@/db/drizzle.schema";
 import { desc } from "drizzle-orm";
 
 class ProblemRepository implements IProblemRepository {
+  
+  /**
+   * Get the top upvoted problems.
+   * When two problems have the same upvote count,
+   * they are sorted in descending order by created_at (newest first).
+   * @param cursor an identifier for the last item fetched.
+   * @param size the number of problems to return per page
+   * @returns an array top <size> upvoted problems
+   */
   async getTopUpvoted(cursor: number, size: number) {
     const problems = await db
       .select()
       .from(problemTable)
-      .orderBy(desc(problemTable.upvote_count))
+      .orderBy(desc(problemTable.upvote_count), desc(problemTable.created_at))
       .limit(size)
       .offset(cursor);
 
