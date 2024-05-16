ad15a0b77b00ca0c2c39d98080eb4e46c8905a73
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-05-17 06:54:16 +0700

    chore: enable to connect to turso db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,15 @@
 import type { Config } from 'drizzle-kit';
 
 const DATABASE_URL = process.env.DATABASE_URL || 'http://127.0.0.1:8080';
+const DATABASE_AUTH_TOKEN = process.env.DATABASE_AUTH_TOKEN || '';
 
 export default {
   schema: './src/db/drizzle.schema.ts',
   out: './src/db/migrations',
   dialect: 'sqlite',
+  driver: 'turso',
   dbCredentials: {
     url: DATABASE_URL,
+    authToken: DATABASE_AUTH_TOKEN
   },
 } satisfies Config;
